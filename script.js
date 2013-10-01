6727a870a679a79ef520a6af0ed1ed5768156e27
Richard Davis <rdavis.bacs@gmail.com>
2013-10-01 12:07:03 -0400

    working

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-$(document).ready(function() {
-  $('button').click(function() {
-    var num1 = parseInt($('#number-1').val());
-    var num2 = parseInt($('#number-2').val());
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
-    if(isNaN(num1) || isNaN(num2)) {
-      $('button').after('<p>you must enter a number</p>')
-    } else {
-      $('#result').val(num1 + num2);
-    };
+function isOperator(s) {
+  operators = [ '+', '-', '/', '*' ];
+  return (operators.indexOf(s) >= 0);
+}
+
+function calculate(num1, num2, op) {
+  if (op === '+') { return (num1 + num2); }
+  else if (op === '-') { return (num1 - num2); }
+  else if (op === '/') { return (num1 / num2); }
+  else if (op === '*') { return (num1 * num2); }
+}
+
+$(document).ready(function() {
+  var register = 0;
+  var operator = '';
+
+  $('table.calculator button').click(function(e) {
+    var buttonPressed = $(e.currentTarget).html();
+    var $display = $('input#display');
+
+    if (isNumeric(buttonPressed)) {
+      $display.val($display.val() + buttonPressed);
+
+    } else if (isOperator(buttonPressed)) {
+      register = parseFloat($display.val());
+      operator = buttonPressed;
+      $display.val('');
+
+    } else if (buttonPressed === '=') {
+      $display.val(calculate(parseFloat(register), parseFloat($display.val()), operator));
+
+    } else if (buttonPressed === 'C') {
+      register = 0;
+      operator = '';
+      $display.val('');
+    }
+
   });
 });
